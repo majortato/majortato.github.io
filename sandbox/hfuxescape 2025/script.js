e14a75e128937fb70e365ed6f98edc1f76fd8dbc
david_wan <wany6@medtronic.com>
2025-12-01 13:49:12 -0600

    2025 hunt

---
+++ sandbox/hfuxescape 2025/script.js
@@ -0,0 +1,39 @@
+  const pc = "MTIyNTY3"; 
+  const pc2 = "MTIxMTI1";
+  const realpc = "dW5sb2NrZWQuaHRtbA=="; 
+  const realpc2 = "dHJ5YWdhaW4uaHRtbA=="
+
+  let input = "";
+
+  function updateDisplay() {
+    document.getElementById("display").textContent =
+      input.padEnd(6, "_");
+  }
+
+  function pressKey(num) {
+    if (input.length < 6) {
+      input += num;
+    }
+
+    updateDisplay();
+
+    if (input.length === 6) {
+      checkCode();
+    }
+  }
+
+  function clearPin() {
+    input = "";
+    updateDisplay();
+  }
+
+  function checkCode() {
+    if (input === atob(pc2)) {
+      window.location.href = atob(realpc2);
+    } else if(input === atob(pc)) {
+      window.location.href = atob(realpc);
+    } else {
+      alert("Incorrect code. Try again!");
+      clearPin();
+    }
+  }
